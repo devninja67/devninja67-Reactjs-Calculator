576a93d16afe41163e361cd7793a656fc57c10c1
devninja67 <devninja67@gmail.com>
2018-10-31 20:50:08 +0800

    importing CalculatorEngine.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './components/header';
-// import Calculator from './Calculator/Calculator';
+import Calculator from './components/Calculator/Calculator';
 
 const App = () => (
-    <div style={{overflow: 'hidden'}}>
-        <Header title='React Calc' />
-        <div className="mt-md-5">
-          {/* <Calculator /> */}
-        </div>
+  <div style={{overflow: 'hidden'}}>
+    <Header title='React Calc' />
+    <div className="mt-md-5">
+      {/*<Calculator />*/}
     </div>
+  </div>
 );
 
 export default App;
