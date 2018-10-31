d7b6ca9d5e6d6a5de36fd7dcec9f97d37e81444b
devninja67 <devninja67@gmail.com>
2018-10-31 21:07:42 +0800

    importing Keypad

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Display from './Display';
 import History from './History';
+import Keypad from './Keypad';
 import CalculatorEngine from './CalculatorEngine';
 
 const calculator = new CalculatorEngine();
