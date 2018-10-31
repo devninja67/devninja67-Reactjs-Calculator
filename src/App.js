4f3117a224af4b2a24c4434decf5f98d5810831b
devninja67 <devninja67@gmail.com>
2018-10-31 21:37:04 +0800

    importing ControlPanel and completing Calculator UI and Logic

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => (
   <div style={{overflow: 'hidden'}}>
     <Header title='React Calc' />
     <div className="mt-md-5">
-      {/*<Calculator />*/}
+      <Calculator />
     </div>
   </div>
 );
