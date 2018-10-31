580abcd312b1bcb176276d7a9af9d9c6e970ff4a
devninja67 <devninja67@gmail.com>
2018-10-31 21:01:11 +0800

    importing history

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Display from './Display';
+import History from './History';
 import CalculatorEngine from './CalculatorEngine';
 
 const calculator = new CalculatorEngine();
